a3168de48fbe6966d21a97da608d595c2eabc8f5
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-06-17 15:55:23 -0400

    Ajuste Puerto

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { storage } from '../utils/storage';
 // =======================================================
 // 1. MICROSERVICIOS YA MOGRADOS AL API GATEWAY
 // =======================================================
-const GATEWAY_URL = import.meta.env.VITE_API_GATEWAY_URL ?? 'http://localhost:8080';
+const GATEWAY_URL = import.meta.env.VITE_API_GATEWAY_URL ?? 'http://localhost:8000';
 
 export const localizacionApi = axios.create({
   baseURL: GATEWAY_URL, 
